5506715b5954d104826e3f9ce0fbc85e5c739373
Jeremy <jpojeremyortega@live.com>
2021-09-29 08:34:20 -0600

    setup panels for SFBA Form

--- SimpleFamilyBudgetApp v 1.0.0/Form1.Designer.cs
+++ SimpleFamilyBudgetApp v 1.0.0/Form1.Designer.cs
@@ -32,42 +32,169 @@ namespace SimpleFamilyBudgetApp_v_1._0._0
             this.panelTopAccountSummary = new System.Windows.Forms.Panel();
             this.panelLeftFilterBar = new System.Windows.Forms.Panel();
             this.panelleftFilterCloseOpen = new System.Windows.Forms.Panel();
+            this.buttonLeftFilterHide = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.panelMainDisplayFullBackground = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panelLeftMainTop = new System.Windows.Forms.Panel();
+            this.panelRightMainTop = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panelleftMainBottom = new System.Windows.Forms.Panel();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.panelRIghtMainBottom = new System.Windows.Forms.Panel();
+            this.panelTopAccountSummary.SuspendLayout();
+            this.panelleftFilterCloseOpen.SuspendLayout();
+            this.panelMainDisplayFullBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelTopAccountSummary
             // 
+            this.panelTopAccountSummary.Controls.Add(this.toolStrip1);
             this.panelTopAccountSummary.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTopAccountSummary.Location = new System.Drawing.Point(0, 0);
             this.panelTopAccountSummary.Name = "panelTopAccountSummary";
-            this.panelTopAccountSummary.Size = new System.Drawing.Size(1602, 95);
+            this.panelTopAccountSummary.Size = new System.Drawing.Size(1602, 110);
             this.panelTopAccountSummary.TabIndex = 0;
             // 
             // panelLeftFilterBar
             // 
             this.panelLeftFilterBar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeftFilterBar.Location = new System.Drawing.Point(0, 95);
+            this.panelLeftFilterBar.Location = new System.Drawing.Point(0, 110);
             this.panelLeftFilterBar.Name = "panelLeftFilterBar";
-            this.panelLeftFilterBar.Size = new System.Drawing.Size(343, 836);
+            this.panelLeftFilterBar.Size = new System.Drawing.Size(343, 821);
             this.panelLeftFilterBar.TabIndex = 1;
             // 
             // panelleftFilterCloseOpen
             // 
+            this.panelleftFilterCloseOpen.Controls.Add(this.buttonLeftFilterHide);
             this.panelleftFilterCloseOpen.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelleftFilterCloseOpen.Location = new System.Drawing.Point(343, 95);
+            this.panelleftFilterCloseOpen.Location = new System.Drawing.Point(343, 110);
             this.panelleftFilterCloseOpen.Name = "panelleftFilterCloseOpen";
-            this.panelleftFilterCloseOpen.Size = new System.Drawing.Size(40, 836);
+            this.panelleftFilterCloseOpen.Size = new System.Drawing.Size(40, 821);
             this.panelleftFilterCloseOpen.TabIndex = 2;
+            // 
+            // buttonLeftFilterHide
+            // 
+            this.buttonLeftFilterHide.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonLeftFilterHide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLeftFilterHide.Location = new System.Drawing.Point(0, 0);
+            this.buttonLeftFilterHide.Name = "buttonLeftFilterHide";
+            this.buttonLeftFilterHide.Size = new System.Drawing.Size(40, 821);
+            this.buttonLeftFilterHide.TabIndex = 0;
+            this.buttonLeftFilterHide.Text = "<";
+            this.buttonLeftFilterHide.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1602, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // panelMainDisplayFullBackground
+            // 
+            this.panelMainDisplayFullBackground.Controls.Add(this.splitContainer1);
+            this.panelMainDisplayFullBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMainDisplayFullBackground.Location = new System.Drawing.Point(383, 110);
+            this.panelMainDisplayFullBackground.Name = "panelMainDisplayFullBackground";
+            this.panelMainDisplayFullBackground.Size = new System.Drawing.Size(1219, 821);
+            this.panelMainDisplayFullBackground.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panelleftMainBottom);
+            this.splitContainer1.Panel1.Controls.Add(this.splitter1);
+            this.splitContainer1.Panel1.Controls.Add(this.panelLeftMainTop);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panelRIghtMainBottom);
+            this.splitContainer1.Panel2.Controls.Add(this.splitter2);
+            this.splitContainer1.Panel2.Controls.Add(this.panelRightMainTop);
+            this.splitContainer1.Size = new System.Drawing.Size(1219, 821);
+            this.splitContainer1.SplitterDistance = 602;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panelLeftMainTop
+            // 
+            this.panelLeftMainTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLeftMainTop.Location = new System.Drawing.Point(0, 0);
+            this.panelLeftMainTop.Name = "panelLeftMainTop";
+            this.panelLeftMainTop.Size = new System.Drawing.Size(602, 415);
+            this.panelLeftMainTop.TabIndex = 0;
+            // 
+            // panelRightMainTop
+            // 
+            this.panelRightMainTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelRightMainTop.Location = new System.Drawing.Point(0, 0);
+            this.panelRightMainTop.Name = "panelRightMainTop";
+            this.panelRightMainTop.Size = new System.Drawing.Size(613, 415);
+            this.panelRightMainTop.TabIndex = 0;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 415);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(602, 8);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // panelleftMainBottom
+            // 
+            this.panelleftMainBottom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelleftMainBottom.Location = new System.Drawing.Point(0, 423);
+            this.panelleftMainBottom.Name = "panelleftMainBottom";
+            this.panelleftMainBottom.Size = new System.Drawing.Size(602, 398);
+            this.panelleftMainBottom.TabIndex = 2;
+            // 
+            // splitter2
+            // 
+            this.splitter2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter2.Location = new System.Drawing.Point(0, 415);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(613, 10);
+            this.splitter2.TabIndex = 1;
+            this.splitter2.TabStop = false;
+            // 
+            // panelRIghtMainBottom
+            // 
+            this.panelRIghtMainBottom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRIghtMainBottom.Location = new System.Drawing.Point(0, 425);
+            this.panelRIghtMainBottom.Name = "panelRIghtMainBottom";
+            this.panelRIghtMainBottom.Size = new System.Drawing.Size(613, 396);
+            this.panelRIghtMainBottom.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1602, 931);
+            this.Controls.Add(this.panelMainDisplayFullBackground);
             this.Controls.Add(this.panelleftFilterCloseOpen);
             this.Controls.Add(this.panelLeftFilterBar);
             this.Controls.Add(this.panelTopAccountSummary);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panelTopAccountSummary.ResumeLayout(false);
+            this.panelTopAccountSummary.PerformLayout();
+            this.panelleftFilterCloseOpen.ResumeLayout(false);
+            this.panelMainDisplayFullBackground.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -77,6 +204,16 @@ namespace SimpleFamilyBudgetApp_v_1._0._0
         private System.Windows.Forms.Panel panelTopAccountSummary;
         private System.Windows.Forms.Panel panelLeftFilterBar;
         private System.Windows.Forms.Panel panelleftFilterCloseOpen;
+        private System.Windows.Forms.Button buttonLeftFilterHide;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Panel panelMainDisplayFullBackground;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panelleftMainBottom;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panelLeftMainTop;
+        private System.Windows.Forms.Panel panelRIghtMainBottom;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Panel panelRightMainTop;
     }
 }
 
